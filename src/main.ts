dd7537e8f78b955d84d109a386a85baef3cd5290
Ihsan <me@isan.eu.org>
2025-12-14 15:35:46 +0800

    Add CORS middleware to the Hono app
    Still need CORS in development

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 import uploadApp from './upload'
 import indexHtml from '../public/index.html' with { type: 'text' }
 
 const PORT: number = Number(process.env.PORT) || 3300
 const app = new Hono()
+app.use(cors())
 app.route('/upload', uploadApp)
 app.get('/', (c) => {
   return c.html(indexHtml)
